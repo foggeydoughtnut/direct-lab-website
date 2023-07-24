1174ab210b865ffd7b5ac36d538828113a065f24
Jeff Anderson <janderson@komodosystems.com>
2023-07-24 09:01:20 -0600

    decision intelligence page complete

---
+++ pages/decision-intelligence/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex justify-center mt-8">
+    <div class="w-2/3 md:text-center text-left">
+      <h1 class="text-5xl font-semibold text-blue-500 md:mt-8 mt-4">Decision Intelligence</h1>
+      <p class="md:mt-16 mt-8 text-left">Almost all projects in our lab fall within the realm of decision intelligence: the art of
+        building intelligence hierarchies capable of making decisions with limited information. At times, these
+        hierarchies may choose to collect more data (explore), generalize based on current understanding (exploit), or
+        augment decisions based on prediction (infer). The following projects are considered foundational decision
+        intelligence.
+      </p>
+
+      <div class="grid md:grid-cols-2 grid-cols-1 md:gap-8 gap-2 mt-16">
+        <img src="/Placeholder-Image.png" />
+        <div>
+          <h1 class="text-2xl font-semibold text-left text-blue-500 md:mt-4 mt-0">Reinforcement Learning</h1>
+          <p class="text-left md:mt-8 mt-2">Reinforcement Learning is a way where intelligent agents can learn for
+            themselves. (Agents can be a computer programs, robots, humans, etc.) This is a machine learning paradigm
+            where agents take actions in an environment (real or virtual) and are rewarded based on their performance. At
+            first, these agents will make many random (and frankly silly) choices, but we'll, over time, be "reinforced"
+            through rewards for good decisions made. Our research seeks to improve the state-of-the-art in modern
+            reinforcement learning. </p>
+        </div>
+
+        <div>
+          <h1 class="text-2xl font-semibold text-left text-blue-500 md:mt-4 mt-0">Learning Heuristics for Motion Planning
+          </h1>
+          <p class="text-left md:mt-8 mt-2">Searching a space is often exhaustive, in other words, it takes a computer one
+            time to think of each and every possibility where something can be. Often, a guiding function, or heuristic,
+            can speed up the search; however, a poorly chosen heuristic can be detrimental by giving us a less than ideal
+            solution. This research uses machine learning and modern ai tools to build heuristic functions suitable for
+            high-speed searching, without losing optimality.</p>
+          </div>
+          <img src="/Placeholder-Image.png" class="mt-16 md:mt-0" />
+
+        <img src="/Placeholder-Image.png" class="mt-16 md:mt-0" />
+        <div>
+          <h1 class="text-2xl font-semibold text-left text-blue-500 md:mt-4 mt-0">Decisionomics (economic decisioning)
+          </h1>
+          <p class="text-left md:mt-8 mt-2">Economic policy-making is complicated. Often, one must consider social
+            impacts: health, incomes, disparate equity, products, capital, labor, and almost anything else you can think
+            of in a complex human system. Oftentimes, information comes slowly, randomly fluctuates, and new data can
+            quickly disrupt old models and thinking. This research deals with core concepts of decision-making under
+            uncertainty for financial risk and social systems.</p>
+        </div>
+      </div>
+
+
+  </div>
+</div></template>
