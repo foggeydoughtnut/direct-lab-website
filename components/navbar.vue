54980d526944043e8981c8f7940c1a8c17f88ea0
foggeydoughtnut <foggeydoughtnut@gmail.com>
2023-07-26 14:43:15 -0600

    make it so the nav bar is more mobile friendly

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="flex flex-row border-b shadow-md py-4">
-    <div class="flex-1 ml-4">
-      <NuxtLink to="/"
-        class=" rounded-xl p-2 px-6 hover:shadow-lg transition-all delay-75 duration-300 text-lg"
-      >
-      Home
-      </NuxtLink>
-    </div>
-    <div>
-      <NuxtLink to="/team"
-        class=" rounded-xl p-2 px-6  hover:shadow-lg transition-all delay-75 duration-300 text-lg"
-      >
-      Team
-      </NuxtLink>
-    </div>
-    <div class="mr-4">
-      <NuxtLink to="/courses"
-        class=" rounded-xl p-2 px-6  hover:shadow-lg transition-all delay-75 duration-300 text-lg"
-      >
-      Courses
-      </NuxtLink>
+  <div>
+    <div class="flex flex-row border-b shadow-md py-4"> <!-- Desktop Mode -->
+      <div class="flex-1 ml-4">
+        <NuxtLink to="/" class=" rounded-xl p-2 md:px-6 hover:shadow-lg transition-all delay-75 duration-300 text-lg">
+          Home
+        </NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/team" class=" rounded-xl p-2 md:px-6  hover:shadow-lg transition-all delay-75 duration-300 text-lg">
+          Team
+        </NuxtLink>
+      </div>
+      <div class="mr-4">
+        <NuxtLink to="/courses"
+          class=" rounded-xl p-2 md:px-6 hover:shadow-lg transition-all delay-75 duration-300 text-lg">
+          Courses
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+</script>
