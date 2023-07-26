910be016bb32f1e1158629d7ba8c37cf146d5248
foggeydoughtnut <foggeydoughtnut@gmail.com>
2023-07-26 09:52:32 -0600

    started on the team page

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    Team Page
+    <div class="flex justify-center mt-8">
+      <div class="w-2/3 text-left">
+        <h1 class="text-2xl font-semibold text-blue-500 md:mt-8 mt-4 md:text-center">DIRECT Lab Team</h1>
+      
+        <div>
+
+          <h2 class="text-xl font-semibold text-left">Leadership</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
